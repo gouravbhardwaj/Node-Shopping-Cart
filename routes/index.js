0967d5174931ad28ed2b941046430911a6e47616
Gourav <gouravrocks247@gmail.com>
2017-09-01 20:11:45 +0530

    mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var Product = require('../models/product');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+  Product.find(function(err,docs){
+     var ProductChunks = new Array();
+     var chunkSize = 3;
+
+     for(var i=0;i<docs.length;i+=chunkSize){
+       ProductChunks.push(docs.slice(i,i+chunkSize));
+       console.log('### : '+i);
+     }
+     console.log('### : ProductChunks '+ProductChunks);
+
+      res.render('shop/index', { title: 'Shopping Cart', products : docs });
+  });
+
 });
 
 module.exports = router;
